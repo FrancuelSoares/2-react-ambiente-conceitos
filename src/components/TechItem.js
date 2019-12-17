a95905f6f979374ac7c2002d8344d5ea075e44cd
Françuel Soares <fs.francuelsoares@gmail.com>
2019-12-17 17:20:22 -0300

    0.8.0
    # New - Added Properties to Component

---
+++ src/components/TechItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// { tech } ou this.props.tech
+function TechItem({ tech, onDelete }) {
+  return (
+    <li>
+      {tech}
+      <button onClick={onDelete} type="button">Remover</button>
+    </li>
+  );
+}
+
+export default TechItem;
